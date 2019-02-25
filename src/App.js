ba56cc32c85f88a2a2f1ab529567a7b68cd14921
OllyDS <desousa88@gmail.com>
2019-02-25 19:40:28 +0000

    played around with combining d3 with react. Next up is understanding d3.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import BarChart from './partOne/barchart/BarChart'
+import WorldMap from './partOne/worldMap/WorldMap'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          <h2>
+            d3 dashboard
+          </h2>
+        </div>
+        <div>
+          {/* <BarChart data={[5, 10, 1, 3]} size={[500, 500]} /> */}
+          <WorldMap data={[5, 10, 1, 3]} size={[500, 500]} />
+        </div>
       </div>
     );
   }
